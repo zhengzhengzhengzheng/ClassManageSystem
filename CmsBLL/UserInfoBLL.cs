bc1c65902a158e4e37b70854c7776b48335de82e
zhengzhengzhengzheng <38465619+zhengzhengzhengzheng@users.noreply.github.com>
2018-12-22 13:25:35 +0800

    Add files via upload

---
+++ CmsBLL/UserInfoBLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CmsDAL;
+using CmsModel;
+using CmsCommon;
+
+namespace CmsBLL
+{
+    public class UserInfoBLL
+    {        
+        UserInfoDAL udDal = new UserInfoDAL();
+
+        public bool Add(UserInfo ui)
+        {
+            return udDal.Insert(ui) > 0;
+        }
+
+        public LoginState login(string name, string pwd)
+        {
+            UserInfo ui = udDal.getByName(name);
+            if (ui == null)
+            {
+                return LoginState.UserNameError;
+            }
+            else
+            {
+                if (ui.uPwd == Md5Helper.EncryptString(pwd))
+                {
+                    //密码正确
+                    return LoginState.Ok;
+                }
+                else
+                {
+                    return LoginState.PasswordError;
+                }
+            }
+        }
+    }
+}
